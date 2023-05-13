817a090b065625ea799bcb8c9e9c08c114345ef5
NeO - Wesley Souza <65511208+neopromic@users.noreply.github.com>
2023-05-13 14:06:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const location = document.getElementById("card-01")
+const modal = document.getElementById("dialog")
+
+ location.onclick = function {
+  dialog.modalShow()
+  setTimeout(timeout, 1000)
+}
+function timeout() {
+  dialog.close()
+}
